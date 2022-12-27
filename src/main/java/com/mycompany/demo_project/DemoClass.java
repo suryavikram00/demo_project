945d950f7f57e8cadcf1ec777b3f01347d8af2d5
surya_vikram <surya.vikram@netmeds.com>
2022-12-28 00:15:23 +0530

    initial commit

---
+++ src/main/java/com/mycompany/demo_project/DemoClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.demo_project;
+
+/**
+ *
+ * @author NMSLAP570
+ */
+public class DemoClass {
+
+    public static void main(String[] args) {
+        User user = new User("surya");
+        System.out.println(user.getFirstName());
+    }
+
+}
---
+++ src/main/java/com/mycompany/demo_project/User.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.demo_project;
+
+/**
+ *
+ * @author NMSLAP570
+ */
+public class User {
+    
+    private String firstName;
+
+    public User(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    
+    
+}
